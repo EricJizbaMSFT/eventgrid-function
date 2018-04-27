6cff068342440fd8989b6622c6ccd490628ae15d
Eric Jizba <erijiz@microsoft.com>
2018-04-27 14:26:18 -0700

    Fix http

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -1,5 +1,23 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(JSON.stringify(req));
+
+    var messages = req.body;
+    // If the request is for subscription validation, send back the validation code.
+    if (messages.length > 0 && messages[0].eventType == "Microsoft.EventGrid.SubscriptionValidationEvent") {
+        context.log('Validate request received');
+        context.res = { status: 200, body: messages[0].data.validationCode }
+    }
+    else {
+        // The request is not for subscription validation, so it's for one or more events.
+        for (var i = 0; i < messages.length; i++) {
+            // Handle one event.
+            var message = messages[i];
+            context.log('Subject: ' + message.subject);
+            context.log('Time: ' + message.eventTime);
+            context.log('Data: ' + JSON.stringify(message.data));
+        }
+
+        context.log(JSON.stringify(req, undefined, 2));
+    }
     context.done();
 };
